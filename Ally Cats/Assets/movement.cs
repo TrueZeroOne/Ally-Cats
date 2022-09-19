09ec0ef1af6a50b4a1af106c5ed352245f99f6bd
TrueZeorOne <blaabjerg.anton@gmail.com>
2022-09-19 15:00:42 +0200

    Jump and shadow

--- Ally Cats/Assets/movement.cs
+++ Ally Cats/Assets/movement.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Rigidbody rb;
     private float horizontalInput;
     private float verticalInput;
     [SerializeField] private float horSpeed;
     [SerializeField] private float verSpeed;
+    [SerializeField] private bool jumpCheck = true;
+    [SerializeField] private float jumpForce;
 
     void Update()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCheck)
+        {
+            Jump();
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontalInput * horSpeed, verticalInput * verSpeed);
+        rb.velocity = new Vector3(horizontalInput * horSpeed, rb.velocity.y, verticalInput * verSpeed);
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
     }
 }
