638e29ea162074532e5bb9f02337e9827deee7a4
TrueZeorOne <blaabjerg.anton@gmail.com>
2022-10-03 12:37:35 +0200

    Update 1

---
+++ Ally Cats/Assets/Scripts/navEnemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class navEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject playerObj;
+    [SerializeField] private NavMeshAgent Agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Agent.destination = playerObj.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Agent.destination = playerObj.transform.position;
+        
+    }
+}
