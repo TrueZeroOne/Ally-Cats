365be2dbc20c40c762e750e3dc87b987fa5d82a7
N7br019 <lucas_olsen@hotmail.dk>
2022-11-17 09:47:59 +0100

    ASA

--- Ally Cats/Assets/Scripts/StartGame.cs
+++ Ally Cats/Assets/Scripts/StartGame.cs
@@ -10,17 +10,6 @@ public class StartGame : MonoBehaviour
 
     public bool useIntegerToLoadLevel;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void StartLvl()
     {
         if (useIntegerToLoadLevel)
